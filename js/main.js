322a67d687b6a43114b8a91b38889cc7cd9a1eac
cochy98 <cosmoferrigno@gmail.com>
2022-03-05 19:11:54 +0100

    aggiunti livelli di difficoltà (JS)

--- js/main.js
+++ js/main.js
@@ -7,14 +7,41 @@
     Quando l'utente clicca su ogni cella, la cella cliccata si colora di azzurro.
  */
 
+// Mi richiamo il bottone tramite 'querySelector'
+const playBtn = document.querySelector('#play');
 
-// Vado a richiamare tramite ID il div 'grid'
-const grid = document.getElementById('grid');
+// Aggiungo un evento al click del bottone
+playBtn.addEventListener('click', function(){
 
-// Ciclo per 100 volte e aggiungo ogni volta una nuova 'cell' alla 'grid'
-for (let i = 1; i <= 100; i++){
-    grid.appendChild(createNewCell(i));
-} 
+    // Quando clicco sul bottone seleziono il livello di difficoltà associato
+    const levelDifficult = document.getElementById('level-difficult').value;
+    // E ne visualizzo il valore in console
+    console.log(`Livello di difficoltà selezionato: ${levelDifficult}`);
+    
+    // Vado a richiamare tramite ID il div 'grid'
+    const grid = document.getElementById('grid');
+
+    // Se il livello di difficoltà è uguale a 'easy'
+    if(levelDifficult === 'easy'){
+        // Ciclo per 100 volte e aggiungo ogni volta una nuova 'cell' alla 'grid'
+        for (let i = 1; i <= 100; i++){
+            grid.appendChild(createNewCell(i));
+        }
+    } else if(levelDifficult === 'medium'){
+        // Ciclo per 81 volte e aggiungo ogni volta una nuova 'cell' alla 'grid'
+        for (let i = 1; i <= 81; i++){
+            grid.appendChild(createNewCell(i));
+        }
+    } else if(levelDifficult === 'strong'){
+        // Ciclo per 49 volte e aggiungo ogni volta una nuova 'cell' alla 'grid'
+        for (let i = 1; i <= 49; i++){
+            grid.appendChild(createNewCell(i));
+        }
+    }
+});
+
+
+
 
 
 
